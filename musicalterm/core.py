0c4fe514107fb47c6a3a3276aa034082cef52c1c
Anas <Codecrusader07@gmail.com>
2025-10-11 18:42:41 +0530

    Refactor video_stream_url function to handle errors and support unsupported music sources

--- musicalterm/core.py
+++ musicalterm/core.py
@@ -1,9 +1,17 @@
 import subprocess
-def video_stream_url(url,music_source="yt"):
-    if music_source.lower() == "yt":
-        video_url = url
-        stream_url = subprocess.check_output(["yt-dlp", "-f", "bestaudio", "-g", video_url]).decode().strip()
-        return stream_url
+
+def video_stream_url(url, music_source="yt"):
+    try:
+        if music_source.lower() == "yt":
+            stream_url = subprocess.check_output(["yt-dlp", "-f", "bestaudio", "-g", url],
+            stderr=subprocess.DEVNULL  # suppress yt-dlp’s noise
+            ).decode().strip()
+            return stream_url
+        else:
+            raise ValueError("Unsupported music source")
+    except subprocess.CalledProcessError:
+        print("Error: Failed to retrieve stream URL.")
+        return None
 
 if __name__ == "__main__":
     stream_url = video_stream_url("https://music.youtube.com/watch?v=BbvRjLPCzJk")
