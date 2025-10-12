77efb77216c4a0625944e57d6c6ea3b624552789
Anas <Codecrusader07@gmail.com>
2025-10-12 10:48:57 +0530

    Refactor UI to enhance display and structure using curses

--- musicalterm/ui.py
+++ musicalterm/ui.py
@@ -1,11 +1,21 @@
 import curses
+from pyfiglet import Figlet # type: ignore
+
+f = Figlet(font='future')
 
 def main(stdscr):
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
     stdscr.clear()
-    stdscr.addstr(0, 0, "Hello, Curses!")
-    stdscr.addstr(1, 0, "Press any key to exit.")
+    height, width = stdscr.getmaxyx()
+    Main_scr = curses.newwin(height, width, 8, 0)
+    Main_scr.clear()
+    stdscr.addstr(f.renderText('Musical term'), curses.color_pair(1))
+
+    Main_scr.addstr(8, 1, "Press any key to exit.")
     stdscr.refresh()
-    stdscr.getch() # Wait for a key press
+    Main_scr.refresh()
+    stdscr.getch()
 
 if __name__ == '__main__':
     curses.wrapper(main)
